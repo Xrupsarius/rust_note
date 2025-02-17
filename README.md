aceb61d51b9a109e0e7c32fdcfb05674e8742a0c
agaev <agaev@iqmen.ru>
2025-02-17 22:20:35 +0300

    remove note menu selection updated find_note function

---
+++ README.md
@@ -0,0 +1,9 @@
+### Note project by rust
+
+Tasks:
+1) Update note function
+2) Get all note function
+3) Update note when file exist by create note function
+4) Add sqlite in future for notes
+5) Remove all notes
+6) Choose any notes by menu (if exists) and update it
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fs, io};
 use std::process::exit;
 use std::fs::File;
 use std::io::Write;
@@ -27,7 +27,7 @@ fn select_menu_num(menu_num: i8) {
     if menu_num == 1 {
         create_new_note();
     } else if menu_num == 2 {
-        find_note();
+        update_note();
     } else if menu_num == 3 {
         get_all_notes();
     } else if menu_num == 4 {
@@ -38,8 +38,20 @@ fn select_menu_num(menu_num: i8) {
     }
 }
 
+fn update_note() {
+    println!("update_note fn");
+}
+
 fn remove_note() {
-    println!("remove_note fn");
+    println!("Enter file name:");
+    let mut file_name = String::new();
+    io::stdin().read_line(&mut file_name).expect("Failed to read line");
+    if find_note(file_name.clone()) {
+        fs::remove_file(&file_name).expect("Failed to delete file");
+        println!("{file_name} successfully deleted.");
+    } else {
+        println!("{file_name} is not found.");
+    }
 }
 
 fn get_all_notes() {
@@ -47,32 +59,30 @@ fn get_all_notes() {
     println!("get_all_notes fn");
 }
 
-fn find_note() {
-    println!("Find note function.");
-    println!("Enter note name for check exists: ");
-    let mut note_name = String::new();
-    io::stdin().read_line(&mut note_name).expect("Failed to read line.");
-    let string = format!("Note with name - {} - has status: {}", &note_name, Path::new(&note_name).exists());
-    println!("{}", string);
+fn find_note(note_name: String) -> bool {
+    return Path::new(&note_name).exists();
 }
 
 fn create_new_note() {
     println!("Enter note name: ");
     let mut note_name = String::new();
     io::stdin().read_line(&mut note_name).expect("Failed to read line.");
-    let mut file = File::create(note_name).expect("Failed to create file.");
-    println!("Enter what you want write to note: ");
-    let mut note_string = String::new();
-    io::stdin().read_line(&mut note_string).expect("Falied to enter string line.");
-    file.write_all(note_string.as_ref()).expect("Error while write to file.");
+    if find_note(note_name.clone()) {
+        println!("Note is exist. Just update it.");
+    } else {
+        let mut file = File::create(&note_name).expect("Failed to create file.");
+        println!("Enter what you want write to note: ");
+        let mut note_string = String::new();
+        io::stdin().read_line(&mut note_string).expect("Failed to enter string line.");
+        file.write_all(note_string.as_ref()).expect("Error while write to file.");
+    }
 }
 
 fn main_menu() {
     println!("What's next: ");
     println!("[1] Create new note");
-    println!("[2] Find note");
+    println!("[2] Update note");
     println!("[3] Get all notes");
     println!("[4] Remove note");
     println!("[5] Exit");
-    // TODO Update note need too
 }
